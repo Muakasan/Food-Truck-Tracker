1c8c2bd0f134c836e4af6fa2d03f4fdf82e7df0b
Christopher <chris.morris55@yahoo.com>
2015-08-01 17:33:01 -0400

    webpage python file edit

--- webpage.py
+++ webpage.py
@@ -6,3 +6,6 @@ app = Flask(__name__, static_folder='/Users/Main_Account/Documents/Homework/Hack
 def home():
     #return 'Index page'
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
