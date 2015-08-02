1dd42f8bf4e3a8716133f2d41e57ef529a92f664
Bopedybopbop <bobhreach@gmail.com>
2015-08-01 21:02:17 -0400

    Rename

--- static/SubmitScript.js
+++ static/SubmitScript.js
@@ -2,6 +2,5 @@ $("#submitButton").click(function(){
 	window.location.replace("../templates/index.html");
 }
 $("#submitButton").click(function(){
-	alert($("#input").val());
 	window.alert("You have successfully signed up!")
 }
