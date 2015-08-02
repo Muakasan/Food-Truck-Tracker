e1d17f0b600ae7a32d8697dc37fe1b941c3321e1
rockout531 <graskovich@gmail.com>
2015-08-01 21:22:09 -0400

    Added a filename-Glenn

--- static/ajax.js
+++ static/ajax.js
@@ -1,4 +1,4 @@
-<script type=text/javascript src="{{url_for('static', filename='') }}"></script>
+<script type=text/javascript src="{{url_for('static', filename='ajax.js') }}"></script>
 
 $(function() {
   $().bind('click', function() {
