a1675624d34923d361b2b8c95b48d45d77e28fb3
Bopedybopbop <bobhreach@gmail.com>
2015-08-01 20:13:46 -0400

    submit

--- static/script.js
+++ static/script.js
@@ -1,9 +1,12 @@
-$("#logIn").click(function(){
+$("#logInButton").click(function(){
 	window.location.replace("login.html");
 }
 $("#signUpButton").click(function(){
 	window.location.replace("signup.html");
 }
-$("#viewMap").click(function(){
+$("#viewMapButton").click(function(){
 	window.location.replace("viewmap.html");
+}
+$("#submitButton").click(function(){
+	window.alert("You have successfully signed up!")
 }
